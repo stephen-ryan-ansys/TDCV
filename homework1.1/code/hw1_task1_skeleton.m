bc4665996bc02348e39f2f9bb459615e518e4126
Stephen Ryan <stephen.ryan@tum.de>
2019-11-09 23:25:51 +0100

    finished hw1task1

--- homework1.1/code/hw1_task1_skeleton.m
+++ homework1.1/code/hw1_task1_skeleton.m
@@ -88,7 +88,11 @@ for i=1:num_files
          s, fy,  0;
         cx, cy,  1;
     ];
-    camera_params = cameraParameters('IntrinsicMatrix', intrinsic_matrix);
+    img_res = size(imread(Filenames{i}), 1:2);
+    camera_params = cameraParameters(...
+        'IntrinsicMatrix', intrinsic_matrix,...
+        'ImageSize', img_res...
+    );
 %   - Define max_reproj_err - take a look at the documentation and
 %   experiment with different values of this parameter 
     max_reproj_err = 4;
@@ -156,7 +160,8 @@ hold off;
 % Leave the value of 1000 to retain reasonable computational time for debugging
 % In order to contruct the final SIFT model that will be used later, consider
 % increasing this value to get more SIFT points in your model
-num_samples=1000;
+% num_samples=1000;
+num_samples = 10000;
 size_total_sift_points=num_samples*num_files;
 
 % Visualise cameras and model SIFT keypoints
@@ -166,31 +171,77 @@ hold on
 % Place model's SIFT keypoints coordinates and descriptors here
 model.coord3d = [];
 model.descriptors = [];
-
+model.coord3d = NaN(size_total_sift_points, 3);
+model.descriptors = NaN(128, size_total_sift_points);
+points_found = 0;
 
 for i=1:num_files
     
 %     Randomly select a number of SIFT keypoints
     perm = randperm(size(keypoints{i},2)) ;
     sel = perm(1:num_samples);
-    
-%    Section to be deleted starts here
-    P = cam_intrinsics.IntrinsicMatrix.'*[cam_in_world_orientations(:,:,i) -cam_in_world_orientations(:,:,i)*cam_in_world_locations(:,:,i).'];
+
+    % P = K[R | t] = [Q | q]
+    P = camera_params.IntrinsicMatrix.'*[cam_in_world_orientations(:,:,i) -cam_in_world_orientations(:,:,i)*cam_in_world_locations(:,:,i).'];
     Q = P(:,1:3);
     q = P(:,4);
     orig = -inv(Q)*q; % this corresponds to C
-%    Section to be deleted ends here
 
+    % setup vertices for TriangleRayIntersection
+    num_faces = size(faces, 1);
+    vert1 = zeros(num_faces, 3);
+    vert2 = zeros(num_faces, 3);
+    vert3 = zeros(num_faces, 3);
+    for k = 1:num_faces
+        face = faces(k, :);
+        % +1 because indices start at 1
+        triangle = vertices(face + 1, :);
+        vert1(k, :) = triangle(1, :);
+        vert2(k, :) = triangle(2, :);
+        vert3(k, :) = triangle(3, :);
+    end
+
+    m = ones(3, 1);
     for j=1:num_samples
-        
     % TODO: Perform intersection between a ray and the object
     % You can use TriangleRayIntersection to find intersections
     % Pay attention at the visible faces from the given camera position
-    
-    end  
+        % m is a point in the image plane
+        m(1:2) = keypoints{i}(1:2, sel(j));
+%         lambda = norm(m(1:2));
+%         ray = orig + lambda*Q\m;
+        ray = orig + Q\m;
+        ray = ray/norm(ray);
+        [intersect, t, u, v, xcoor] = TriangleRayIntersection(...
+            repmat(orig', size(vert1, 1), 1),... %from the func docs
+            repmat(ray',  size(vert1, 1), 1),...
+            vert1, vert2, vert3,...
+            'planeType', 'one sided'... % necessary to ignore occluded faces
+        );
+        % should be only one intersection because 'one_sided'
+        if any(size(find(intersect)) > 1)
+            fprintf('Warning! Encountered a double intersection at file %d, keypoint %d!', i, sel(j));
+        end
         
+        % The point was determined to be on the teabox
+        if any(intersect)
+            points_found = points_found + 1;
+            idx = find(intersect);
+            % M is a point in the world frame
+            M = xcoor(idx, :);
+            % alternate way when ray is unit vector:
+            % t = t(idx);
+            % M = orig + ray*t;
+            % M = M';
+            model.coord3d(points_found, :) = M;
+            model.descriptors(:, points_found) = descriptors{i}(:, sel(j));
+        end
+    end
 end
+model.coord3d = rmmissing(model.coord3d);
+model.descriptors = rmmissing(model.descriptors);
 
+scatter3(model.coord3d(:,1), model.coord3d(:,2), model.coord3d(:,3), '.', 'g');
 hold off
 xlabel('x');
 ylabel('y');
@@ -206,4 +257,3 @@ axis equal;
 xlabel('x');
 ylabel('y');
 zlabel('z');
-
